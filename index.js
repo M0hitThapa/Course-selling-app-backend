7c6d5c0dda02365986de844573b3d3ec85ac253c
Mohit Thapa <thapamohit2058@gmail.com>
2025-05-19 16:09:10 +0545

    add signup and signin route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose  = require("mongoose")
 require('dotenv').config();
 const app = express()
 const port = 3000
+app.use(express.json()); 
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,37 @@
 // const Router = express.Router;
 const { Router } = require("express")
 const userRouter = Router();
+const { userModel } = require("../db")
 
-userRouter.post('/signin', (req,res) => {
+userRouter.post("/signup",async function(req,res) {
+    const {email, password, firstName, lastName} = req.body;
+      await userModel.create({
+        email:email,
+        password:password,
+        firstName:firstName,
+        lastName:lastName,
+        
+    })
+
+   res.json({
+    message:"Signup done"
+   })
+})
+userRouter.post('/signin', async function(req,res) {
+   const {email, password } = req.body;
+
+   const user = await userModel.findOne({
+    email:email,
+    password:password
+   });
+
+   if(user) {
+    res.send("user is there")
+   }
     res.send("login endpoint")
 })
 
-userRouter.post("/signup", (req,res) => {
-    res.send("signup endpoint")
-})
+
 
 userRouter.get("/purchases", (req,res) => {
     res.send("purchased course endpoint")
